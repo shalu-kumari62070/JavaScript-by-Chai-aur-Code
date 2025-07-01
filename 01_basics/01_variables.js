90885a9c73de46213774d1b87272e83467eb257c
Shalu kumari <shalurajput6205@gmail.com>
2025-07-01 13:42:28 +0000

    conatiner config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const password = 123;
+let email = "jslearn@gamil.com";
+var city = "jaipur";
+let account;
+/*
+Prefer not to use var
+because of issue in block scope and functional scopte
+*/
+console.table([password, email, city, account]);
