f4f0dda898e3d52622cd952c7bf3521b18dc381e
Shalu kumari <shalurajput6205@gmail.com>
2025-07-02 13:20:41 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code an newer version
+
+console.log("now we study about data types");
+
+let name = "siya";
+let iscorrect = true;
+let age = 20;
+let state = null;
+let buy; //this is undefined
+
+// number =>2 to power 53
+// bigint (it is used when large integer number than the limit of the Number type.,It is created by adding n at the end of an integer)
+// String => ""
+// boolean =>true/false;
+// null =>standalone value
+// undefined
+// symbol =>unique
+// obeject
+
+console.log(typeof "hello");
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof age);
+console.log(typeof state);
+console.log(typeof buy);
+console.log(typeof iscorrect);
