c2e8454dabf19b7a05b18dac0c5d57b17edd8278
Shalu kumari <shalurajput6205@gmail.com>
2025-07-03 15:03:18 +0000

    convrsionoperation done

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,27 @@
+let score="hello";
+
+console.log(typeof score);
+// or
+console.log(typeof(score));
+let valuenumber = Number(score);
+console.log(typeof valuenumber);
+console.log(valuenumber)
+
+// return
+// "34" => 33
+// "33abc" => NaN
+// null => 0
+// true =>1 ; false => 0;
+
+let islog = 1;
+let booleanislog = Boolean(islog);
+console.log(booleanislog);
+
+// 1 =>true; 0 =>false;
+//  "" => false;
+// "rajput" =>true
+
+let somenumber = 50;
+let stringnumber = String(somenumber);
+console.log(stringnumber);
+console.log(typeof stringnumber);
