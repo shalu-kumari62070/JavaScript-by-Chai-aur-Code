a487436e3e9b1c19f07d74e932615d0e42601b5f
Shalu kumari <shalurajput6205@gmail.com>
2025-07-04 13:28:46 +0000

    done

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,11 +1,11 @@
 let score="hello";
 
-console.log(typeof score);
+// console.log(typeof score);
 // or
-console.log(typeof(score));
+// console.log(typeof(score));
 let valuenumber = Number(score);
-console.log(typeof valuenumber);
-console.log(valuenumber)
+// console.log(typeof valuenumber);
+// console.log(valuenumber)
 
 // return
 // "34" => 33
@@ -15,7 +15,7 @@ console.log(valuenumber)
 
 let islog = 1;
 let booleanislog = Boolean(islog);
-console.log(booleanislog);
+// console.log(booleanislog);
 
 // 1 =>true; 0 =>false;
 //  "" => false;
@@ -23,5 +23,28 @@ console.log(booleanislog);
 
 let somenumber = 50;
 let stringnumber = String(somenumber);
-console.log(stringnumber);
-console.log(typeof stringnumber);
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
+
+// ******************** Opertions *******************
+let value = 3;
+let negvalue = -value;
+console.log(negvalue);
+
+console.log(2+2);
+console.log(4-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2%3);
+
+let str1 = "hello";
+let str2 = "world";
+console.log(str1+str2);
+
+console.log("1" + 2);  // output = 12
+console.log(1 + "2");  // output = 12
+console.log("1" + 2 +2);  // output = 122
+console.log(1 + 2 + "2");  // output = 32
+
+console.log(+true); // output = 1
+console.log(-true); // output = -1
