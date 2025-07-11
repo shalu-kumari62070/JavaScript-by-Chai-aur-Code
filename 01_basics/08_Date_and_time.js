46e8fdb99b102bc0d78166964b61e63cb4eff556
Shalu kumari <shalurajput6205@gmail.com>
2025-07-11 14:36:06 +0000

    date and time

--- 01_basics/08_Date_and_time.js
+++ 01_basics/08_Date_and_time.js
@@ -19,4 +19,27 @@ let myCreateDate3 = new Date(2024, 0, 23, 5, 6);
 // console.log(myCreateDate3.toLocaleString());   // output = 1/23/2024, 5:06:00 AM
 
 let myCreateDate4 = new Date(23, 2, 2024);
-console.log(myCreateDate4.toString());
+// console.log(myCreateDate4.toString());  // output =  Thu Sep 13 1928 00:00:00 GMT+0000 (Coordinated Universal Time)
+
+
+let myCreateDate5 = new Date("2024-02-23");  // "yy-mm-dd"
+// console.log(myCreateDate5.toLocaleString())  // output = 2/23/2024, 12:00:00 AM
+
+let myCreateDate6 = new Date("11-07-2025");  // "mm-dd-yy"
+// console.log(myCreateDate6.toLocaleString()); // output = 11/7/2025, 12:00:00 AM
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp) // this output is in milliseconds so output = 1752243945521
+console.log(myCreateDate6.getTime()) // this output is in milliseconds so output = 1762473600000
+
+console.log(Math.floor(Date.now()/1000));  // this is conversion from millisecond to second we dived by thousand
+// output = 1752244060
+
+let newDate = new Date();
+console.log(newDate.getMonth()) // output = 6
+console.log(newDate.getFullYear()); // output = 2025
+
+// note: toLocaleString is very useful beacuse here we can add more method
+console.log(newDate.toLocaleString('default', {
+    weekday: "long",
+})) // output = 2 Friday
